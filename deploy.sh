51c2ed8e855cf2c17ad0831fbc45c9220aaf6b2b
Udlei Nati <udlei@protonmail.ch>
2017-01-23 15:17:15 +0100

    Excluding node_modules from the package

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ export S3_FILE_PATH=app-rest-service/`date +"%Y-%m"`
 export S3_FILE_NAME=${CIRCLE_SHA1:0:7}.zip
 export EB_VERSION=$CIRCLE_BRANCH-${CIRCLE_SHA1:0:7}
 sed -ie 's/LOCAL/'${CIRCLE_SHA1:0:7}'/g' package.json
-zip -r -9 $S3_FILE_NAME *
+zip -r -9 --exclude=node_modules/* $S3_FILE_NAME *
 aws s3 cp $S3_FILE_NAME s3://redirect-eb-$CIRCLE_BRANCH/$S3_FILE_PATH/
 aws elasticbeanstalk create-application-version --application-name redirectpro \
 	--version-label $EB_VERSION \
