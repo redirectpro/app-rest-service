f687e9c6af28b0d958bdac6bfba4be3b30be79da
Udlei Nati <udlei@protonmail.ch>
2017-01-23 19:14:29 +0100

    Building ES5 version

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ export S3_FILE_PATH=app-rest-service/`date +"%Y-%m"`
 export S3_FILE_NAME=${CIRCLE_SHA1:0:7}.zip
 export EB_VERSION=$CIRCLE_BRANCH-${CIRCLE_SHA1:0:7}
 sed -ie 's/LOCAL/'${CIRCLE_SHA1:0:7}'/g' package.json
+npm run prestart
 zip -r -9 --exclude=node_modules/* $S3_FILE_NAME *
 aws s3 cp $S3_FILE_NAME s3://redirectpro-eb-$CIRCLE_BRANCH/$S3_FILE_PATH/
 aws elasticbeanstalk create-application-version --application-name redirectpro \
