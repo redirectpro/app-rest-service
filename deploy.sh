a91611a63aecab23c5d69408d54d9141e84ee055
Udlei Nati <udlei@protonmail.ch>
2017-04-12 00:07:33 +0200

    Fix bash script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 pip install awsebcli
-aws configure set region ${DEPLOY_AWS_REGION}
+aws configure set region $DEPLOY_AWS_REGION
 mkdir .elasticbeanstalk
 echo 'branch-defaults:
   master:
     environment: null
 global:
-  application_name: ${EB_APPLICATION_NAME}
+  application_name: '$EB_APPLICATION_NAME'
   branch: null
   default_ec2_keyname: null
   default_platform: node.js
-  default_region: ${AWS_REGION}
+  default_region: '$AWS_REGION'
   instance_profile: null
   platform_name: null
   platform_version: null
@@ -18,4 +18,4 @@ global:
   repository: null
   sc: git
   workspace_type: Application' > .elasticbeanstalk/config.yml
-eb deploy ${EB_ENVIRONMENT_NAME}
+eb deploy $EB_ENVIRONMENT_NAME
